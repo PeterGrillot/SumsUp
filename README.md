c37b9eadae759e0df9f28d0fe3b7668eebcb7337
Peter Grillot <petergrillot@gmail.com>
2017-06-16 16:24:57 -0400

    ok fixed!

--- README.md
+++ README.md
@@ -1,22 +1,6 @@
-# electron-boilerplate
+# SumsUp
 
-[![Build Status](https://travis-ci.org/superbrothers/electron-boilerplate.svg?branch=master)](https://travis-ci.org/superbrothers/electron-boilerplate)
-[![Dependency Status](https://gemnasium.com/superbrothers/electron-boilerplate.svg)](https://gemnasium.com/superbrothers/electron-boilerplate)
-
-A electron app template with React, babel, sass, gulp and bower :tada:
-
-## Start developing
-
-```
-% npm install && npm run bower -- install
-% npm run dev
-```
-
-## Build package
-
-```
-% npm run release
-```
+Calculator built with Electron b/c why not!?
 
 ## LICENSE
 
--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,106 +1,100 @@
-"use strict";
-(function() {
-	// Vars
-	const inputDisplay = document.getElementById('js-inputDisplay');
-	const outputDisplay = document.getElementById('js-outputDisplay');
-	const savedDisplay = document.getElementById('js-savedDisplay');
-	const buttonExecute = document.getElementById('js-execute');
-	const buttonRemember = document.getElementById('js-remember');
-	const buttonRecall = document.getElementById('js-recall');
-	const buttonClear = document.getElementById('js-clear');
-	const buttonAllClear = document.getElementById('js-allClear');
-	const buttonNum = document.querySelectorAll('.button__num');
-	const buttonOperant = document.querySelectorAll('.button__operant');
-	let mem = '';
+"use strict"
+import getChar from './char'
 
-	//Functions
-	const evaluate = function(fn) {
-		return new Function('return ' + fn)();
-	};
-	const display = function(num) {
-		inputDisplay.value = inputDisplay.value + num;
-	};
-	const remember = function() {
-		mem = outputDisplay.innerHTML;
-	};
-	const recall = function() {
-		inputDisplay.value = inputDisplay.value + mem;
-	};
-	const clear = function(){
-		inputDisplay.value = '';
-	};
-	const allClear = function(){
-		inputDisplay.value = '';
-		savedDisplay.innerHTML = '';
-	};
-	const backspace = function() {
-		inputDisplay.value = inputDisplay.value.slice(0, -1);
-	};
-	const execute = function() {
-		if (inputDisplay.value !== '') {
-			if (inputDisplay.value[0] === '*' ||
-				inputDisplay.value[0] === '/' ||
-				inputDisplay.value[0] === '+' ||
-				inputDisplay.value[0] === '-') {
-				outputDisplay.innerHTML = evaluate(outputDisplay.innerHTML + inputDisplay.value);
-			} else {
-				outputDisplay.innerHTML = evaluate(inputDisplay.value);
-			}
-			savedDisplay.innerHTML = `<li>${inputDisplay.value} = ${outputDisplay.innerHTML}</li>${savedDisplay.innerHTML}`;
-			inputDisplay.value = '';
+// Vars
+const inputDisplay = document.getElementById('js-inputDisplay')
+const outputDisplay = document.getElementById('js-outputDisplay')
+const savedDisplay = document.getElementById('js-savedDisplay')
+const buttonExecute = document.getElementById('js-execute')
+const buttonRemember = document.getElementById('js-remember')
+const buttonRecall = document.getElementById('js-recall')
+const buttonClear = document.getElementById('js-clear')
+const buttonAllClear = document.getElementById('js-allClear')
+const buttonNum = document.querySelectorAll('.button__num')
+const buttonOperant = document.querySelectorAll('.button__operant')
+let mem = ''
+
+//Functions
+const evaluate = function(fn) {
+	return new Function('return ' + fn)()
+}
+const display = function(num) {
+	inputDisplay.value = inputDisplay.value + num
+}
+const remember = function() {
+	mem = outputDisplay.innerHTML
+}
+const recall = function() {
+	inputDisplay.value = inputDisplay.value + mem
+}
+const clear = function() {
+	inputDisplay.value = ''
+}
+const allClear = function() {
+	inputDisplay.value = ''
+	savedDisplay.innerHTML = ''
+}
+const backspace = function() {
+	inputDisplay.value = inputDisplay.value.slice(0, -1)
+}
+const execute = function() {
+	if (inputDisplay.value !== '') {
+		if (inputDisplay.value[0] === '*' ||
+			inputDisplay.value[0] === '/' ||
+			inputDisplay.value[0] === '+' ||
+			inputDisplay.value[0] === '-') {
+			outputDisplay.innerHTML = evaluate(outputDisplay.innerHTML + inputDisplay.value)
+		} else {
+			outputDisplay.innerHTML = evaluate(inputDisplay.value)
 		}
-
-	};
-
-	for (let i = 0; i < buttonNum.length; i++) {
-		buttonNum[i].addEventListener('click', function(event) {
-			display(event.toElement.dataset.num);
-		});
+		savedDisplay.innerHTML = `<li>${inputDisplay.value} = ${outputDisplay.innerHTML}</li>${savedDisplay.innerHTML}`
+		inputDisplay.value = ''
 	}
-	for (let i = 0; i < buttonOperant.length; i++) {
-		buttonOperant[i].addEventListener('click', function(event) {
-			display(event.toElement.dataset.operant);
-		});
-	}
-	buttonExecute.addEventListener('click', function() {
-		execute();
-	});
-	buttonRemember.addEventListener('click', function() {
-		remember();
-	});
-	buttonRecall.addEventListener('click', function() {
-		recall();
-	});
-	buttonClear.addEventListener('click', function() {
-		clear();
-	});
-	buttonAllClear.addEventListener('click', function() {
-		allClear();
-	});
-	savedDisplay.addEventListener('click',function(evt){
-		this.select();
-	});
-	var turnOnKeyboard = function(){
-		document.addEventListener("keydown", function(evt){
-			evt = evt || window.event;
-			if (
-				(evt.which >= 46 && evt.which <= 57) ||
-				(evt.which >= 96 && evt.which <= 105) ||
-				(evt.which >= 187 && evt.which <= 189) ||
-				evt.which == 43 ||
-				evt.which == 45 ||
-				evt.which == 42 ||
-				evt.which == 61) {
-				display(String.fromCharCode(evt.which));
-			}
-			if (evt.which == 13) {
-				evt.preventDefault;
-				execute();
-			}
-			if (evt.which == 44 || evt.which == 8 ){
-				backspace();
-			}
-		});
-	};
-	turnOnKeyboard();
-})();
+
+}
+
+for (let i = 0; i < buttonNum.length; i++) {
+	buttonNum[i].addEventListener('click', function(event) {
+		display(event.toElement.dataset.num)
+	})
+}
+for (let i = 0; i < buttonOperant.length; i++) {
+	buttonOperant[i].addEventListener('click', function(event) {
+		display(event.toElement.dataset.operant)
+	})
+}
+buttonExecute.addEventListener('click', function() {
+	execute()
+})
+buttonRemember.addEventListener('click', function() {
+	remember()
+})
+buttonRecall.addEventListener('click', function() {
+	recall()
+})
+buttonClear.addEventListener('click', function() {
+	clear()
+})
+buttonAllClear.addEventListener('click', function() {
+	allClear()
+})
+savedDisplay.addEventListener('click', function(evt) {
+	this.select()
+})
+var turnOnKeyboard = function() {
+	document.addEventListener("keydown", function(evt) {
+		evt = evt || window.event
+		let key = getChar(evt.shiftKey, evt.which)
+		if (key) {
+			display(key)
+		}
+		if (evt.which == 13) {
+			evt.preventDefault
+			execute()
+		}
+		if (evt.which == 8) {
+			backspace()
+		}
+	})
+}
+turnOnKeyboard()
